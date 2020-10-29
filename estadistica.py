f50d2818334763483152a602c14784366c719626
UzielLopez <A01733922@itesm.mx>
2020-10-29 01:29:25 +0000

    Correción de errores

--- estadistica.py
+++ estadistica.py
@@ -7,22 +7,22 @@ df = pd.read_csv("ulabox_orders_with_categories_partials_2017.csv")
 print("Tamaño del data set: ", len(df.index), " entradas" )
 
 # %%
-#Imprimir las variables presentes en el dataser
+#Variables presentes en el dataset
 print("Variabless presentes: ")
 for variable in df.columns:
     print(variable)
 
 # %%
 #Verificar máximos y mínimos de las variables (rango)
-for i in range(len(df.columns)):
-    print(str(df.columns[i]) + " --- Máximo: ", df.iloc[i].max(), "Mínimo: ", df.iloc[i].min())
+for i in range(2, len(df.columns)): #Ignoramos las primeras dos columnas, pues no son datos cuantitativos
+    print(str(df.columns[i]) + " --- Máximo: ", df.iloc[:, i].max(), "Mínimo: ", df.iloc[:, i].min())
     print("-"*40)
 
 
 
 # %%
-for i in range(len(df.columns)):
-    print(str(df.columns[i]) + " --- Media: ", (df.iloc[i]).mean(), "Mediana: ", (df.iloc[i]).median(),"Moda: ", (df.iloc[i]).mode()[0])
+for i in range(2, len(df.columns)): #Ignoramos las primeras dos columnas, pues no  son datos cuantitativos
+    print(str(df.columns[i]) + " --- Media: ", (df.iloc[:, i]).mean(), "Mediana: ", (df.iloc[:, i]).median(),"Moda: ", (df.iloc[:, i]).mode()[0])
     print("-"*60)
 
 #print("Mínimos: ", df.min())
